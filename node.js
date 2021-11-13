fa361123abaf36744d69eb3680428d6a2bae9274
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-13 08:46:39 -0800

    class1

--- node.js
+++ node.js
@@ -1,17 +1,12 @@
-class Animal {
+//
 
-    constructor(name) {
-      this.name = name;
-    }
-
-  }
-
-  class Rabbit extends Animal {
-    constructor(name) {
-      super(name);
-      this.created = Date.now();
+class NumsArray extends Array {
+    isEmpty() {
+      return this.length === 0;
     }
   }
-
-  let rabbit = new Rabbit("White Rabbit");
-  console.log(rabbit.name);
+  let arr = new NumsArray(1, 3,  6, 11, 27);
+  console.log(arr.isEmpty());
+  let filterArr = arr.filter(item => item >= 10);
+  console.log(filterArr);
+  console.log(filterArr.isEmpty());
